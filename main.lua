201cb8103e63c97a03f570a42f6a8d49bcc98c74
Cottapina <cottapina@gmail.com>
2021-01-04 19:06:14 -0300

    Initial Commit

---
+++ main.lua
@@ -0,0 +1,18 @@
+function love.load()
+
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+    
+end
